ee489f4879481aaa5cc084071f50226924517b5a
ghabrielsevero <ghabriel.severo@gmail.com>
2018-10-17 16:28:09 -0300

    Screen - MercadoriaEmpresa

---
+++ View/MercadoriaEmpresa.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" charset="utf-8" />
+	<title>4</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="" >
+	<link rel="StyleSheet" href="4.css" />
+	<script src="http://exportkit.com/cdn/js/ek-googlefont-autoloader-v2-min.js"></script>
+	<!-- Add your custom HEAD content here -->
+</head>
+<body>
+	<div id="content-container" >
+
+			<div id="menu" >
+				MENU
+			</div>
+			<ul>
+
+				<li>
+						<div id="group_209"  >
+						<img src="Assets/history.png" id="history" />
+						<div id="rectangle_870"  ></div>
+						</div>
+						<div id="text_3" >
+							Entregas
+						</div>
+				</li>
+				<li>
+						<img src="Assets/basket.png" id="basket" />
+						<div id="text_2" >
+							Mercadorias
+						</div>
+				</li>
+				<li>
+					<div id="mask_group_211"  >
+						<img src="Assets/settings__1_.png" id="settings__1_" />
+						<div id="rectangle_870_1"  ></div>
+						<div id="rectangle_871"  ></div>
+						<div id="text" >
+							Configurações
+						</div>
+					</li>
+				<li>
+						<div id="mask_group_213"  >
+						<img src="Assets/logout_variant.png" id="logout_variant" />
+						<div id="rectangle_870_2"  ></div>
+						</div>
+						<div id="text_1" >
+							Sair
+						</div>
+				</li>
+			</ul>
+			
+		</div>
+		<div id="entregas" >
+			Entregas
+		</div>
+		<div id="lista_de_entregas" >
+			Lista de entregas
+		</div>
+
+		<div id="group_2295"  >
+			<div id="rectangle_929"  ></div>
+			<div id="jose_carlos_santos_" >
+				Jose Carlos Santos
+			</div>
+			<div id="sqn_705_bloco_p____apartamento_204_" >
+				SQN 705 Bloco P -  Apartamento 204
+			</div>
+			<div id="solicitado_as_07_45h" >
+				Solicitado as 07:45h
+			</div>
+			<div id="r__20_00" >
+				R$ 20,00
+			</div>
+
+			<div id="pilula"  >
+				<img src="Assets/path_664.png" id="path_664" />
+
+			</div>
+			<div id="_3" >
+				3
+			</div>
+			<div id="_1" >
+				1
+			</div>
+			<img src="Assets/file_document.png" id="file_document" />
+			<img src="Assets/button_bounds.png" id="button_bounds" />
+
+		</div>
+		<div id="hoje" >
+			HOJE
+		</div>
+
+		<div id="group_2243"  >
+
+			<div id="group_51"  >
+				<div id="rectangle_76"  ></div>
+				<div id="hint_text" >
+					Todos
+				</div>
+
+			</div>
+			<div id="hint_text_1" >
+				Ordem
+			</div>
+
+			<div id="arrow_drop_down_"  >
+				<div id="bounds"  ></div>
+				<img src="Assets/icon.png" id="icon" />
+
+			</div>
+
+		</div>
+
+		<div id="group_2245"  >
+
+			<div id="group_51_1"  >
+				<div id="rectangle_76_1"  ></div>
+
+			</div>
+			<div id="hint_text_2" >
+				Pesquise
+			</div>
+
+			<div id="lupa"  >
+				<img src="Assets/lupa_1.png" id="lupa_1" />
+
+			</div>
+
+		</div>
+		<div id="rectangle_931"  ></div>
+		<div id="rectangle_996"  ></div>
+
+		<div id="group_2296"  >
+			<div id="rectangle_872"  ></div>
+			<img src="Assets/path_2745.png" id="path_2745" />
+			<div id="_8_32_pm" >
+				8:32 PM
+			</div>
+			<div id="hoje__01_de_outubro_de_2018" >
+				Hoje, 01 de Outubro de 2018
+			</div>
+
+		</div>
+
+		<div id="group_2299"  >
+			<div id="rectangle"  ></div>
+			<img src="Assets/path.png" id="path" />
+
+		</div>
+		<div id="rectangle_1618"  ></div>
+		<div id="rectangle_1625"  ></div>
+
+	</div>
+	<script>var specialChars = document.querySelectorAll("span.char"); for(var c=0; c<specialChars.length; c++){ specialChars[c].innerHTML = decodeURIComponent(specialChars[c].innerHTML); }</script>
+
+</body>
+</html>
