9265832b3cc0fd8cd57ea1e2801438c5968dd7f0
ghabrielsevero <ghabriel.severo@gmail.com>
2018-10-17 13:01:34 -0300

    ScreenLogin

---
+++ View/Login.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html" charset="utf-8" />
+  		<title>1</title>
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<meta name="description" content="" >
+  		<link rel="StyleSheet" href="style/stylelogin.css" />
+  		<script src="http://exportkit.com/cdn/js/ek-googlefont-autoloader-v2-min.js"></script>
+  		<!-- Add your custom HEAD content here -->
+</head>
+<body>
+  <div id="content-container" >
+			<div id="_bg___1"  ></div>
+
+			<div id="group_2298"  >
+				<img src="Assets/juan_gomez_711699_unsplash.png" id="juan_gomez_711699_unsplash" />
+				<div id="rectangle_1610"  ></div>
+
+			</div>
+			<img src="Assets/rectangle_1.png" id="rectangle_1" />
+
+			<div id="group_1"  >
+				<div id="supercado_delivery" >
+					SUPERCADO<br/>DELIVERY
+				</div>
+				<div id="cadastre_se_e_fa_a_suas_compras__online_com_comodidade__em_qualquer_hora_e_lugar__evitando_longas_filas_e_tempo_de_espera_" >
+					Cadastre-se e fa<span class="char">%C3%A7a</span>a suas compras  online com comodidade, em qualquer hora e lugar, evitando longas filas e tempo de espera.
+				</div>
+
+			</div>
+			<div id="acessar_sistema" >
+				ACESSAR SISTEMA
+			</div>
+
+			<div id="focus_counter"  >
+				<div id="bounds"  ></div>
+				<div id="divider"  ></div>
+				<div id="e_mail_ou_cpf_cnpj" >
+					E-mail ou CPF/CNPJ
+				</div>
+
+			</div>
+
+			<div id="focus_counter_1"  >
+				<div id="bounds_1"  ></div>
+				<div id="divider_1"  ></div>
+				<div id="digite_sua_senha" >
+					Digite sua senha
+				</div>
+
+			</div>
+
+			<div id="btn_login"  >
+				<div id="rectangle_166"  ></div>
+				<div id="entrar" >
+					ENTRAR
+				</div>
+
+			</div>
+			<img src="Assets/cart.png" id="cart" />
+			<div id="imarket" >
+				iMarket
+			</div>
+
+		</div>
+		<script>var specialChars = document.querySelectorAll("span.char"); for(var c=0; c<specialChars.length; c++){ specialChars[c].innerHTML = decodeURIComponent(specialChars[c].innerHTML); }</script>
+
+</body>
+</html>
