293fb6c1169e1d8e146eaa8684c56000ff052c1e
jhonatasfender <jhonatas.fender@gmail.com>
2018-12-02 05:53:14 -0200

    update

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=faculdade',
     'username' => 'root',
-    'password' => 'P40s3g2015',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
